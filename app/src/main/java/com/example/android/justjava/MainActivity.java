3e38998368ab32e37df664fc6317ef56d26cd3bd
Mofoluwasho Kayode <folushoakinnawo@gmail.com>
2018-06-25 18:54:00 +0100

    changes

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         //display(quantity);
-        price = quantity * 2;
+        price = quantity * 3;
         //displayPrice(price);
         priceMessage = "Total: N" + price + "\nThank you!";
         displayMessage(priceMessage);
